dcae496bf287168ab6beb3e2ba0fa797895341c6
Denis <dkilikki22@gmail.com>
2026-01-14 13:48:35 +0300

    input arguments

---
+++ src/input/mod.rs
@@ -0,0 +1,2 @@
+pub mod arguments;
+pub use arguments::Arguments;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#[allow(dead_code)]
+
+mod input;
+
 fn main() {
-    
+    let args = input::Arguments::new();
+    match args {
+        Ok(val) => println!("Arguments passed : {:?}", val),
+        Err(err) => println!("Error occured {}", err)
+    }
 }
